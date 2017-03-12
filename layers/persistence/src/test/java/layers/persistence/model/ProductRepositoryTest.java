0b0a72505f8eb2e1780c39d3e7bd42affc36e704
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2017-03-12 09:14:56 +0200

    add simple persistence test using in memory db

---
+++ layers/persistence/src/test/java/layers/persistence/model/ProductRepositoryTest.java
@@ -0,0 +1,45 @@
+package layers.persistence.model;
+
+import layers.persistence.config.HibernateConfiguration;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = {HibernateConfiguration.class})
+@RunWith(SpringRunner.class)
+public class ProductRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void shouldAddProduct() {
+
+        productRepository.add(new Product(0, "Cool Beans"));
+        productRepository.add(new Product(0, "Arabica Beans"));
+
+        sessionFactory.getCurrentSession().flush();
+
+        assertThat(countRowsInTableWhere("product", "name = 'Cool Beans'")).isEqualTo(1);
+        assertThat(countRowsInTableWhere("product", "name like '%Beans'")).isEqualTo(2);
+        assertThat(countRowsInTableWhere("product", null)).isEqualTo(2);
+    }
+
+    @Sql(statements = {
+        "insert into product(id, name) values (1, 'Cool Beans'),(2, 'Java Beans');"
+    })
+    @Test
+    public void shouldGetAllProducts() {
+        assertThat(productRepository.getProducts().size()).isEqualTo(2);
+    }
+}
