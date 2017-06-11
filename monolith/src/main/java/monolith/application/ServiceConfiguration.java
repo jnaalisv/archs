486e2fcc07416095c6fb41dbdc1509ea74a9cc66
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2017-06-11 15:41:30 +0300

    remove unnecesary annotation

--- monolith/src/main/java/monolith/application/ServiceConfiguration.java
+++ monolith/src/main/java/monolith/application/ServiceConfiguration.java
@@ -2,11 +2,9 @@ package monolith.application;
 
 import monolith.persistence.PersistenceConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
 @Import(PersistenceConfiguration.class)
 @EnableTransactionManagement
 @ComponentScan("monolith.application.impl")
